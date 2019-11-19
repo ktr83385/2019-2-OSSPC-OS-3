230f1d3077c04d05a786fc49ad03aead324237b6
hyeonazzz <rud0244@gmail.com>
2019-11-19 21:02:25 +0900

    add bgm

--- Tetris-master/src/com/ok/main/Main.java
+++ Tetris-master/src/com/ok/main/Main.java
@@ -1,4 +1,10 @@
 package com.ok.main;
+import java.io.File;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
 
 public class Main {
 
@@ -7,6 +13,16 @@ public class Main {
 	
 	public static void main(String[] args) {
 		new TMain();
+
+		BGM sound_bgm = new BGM();
+		while(true) {
+			try {
+				sound_bgm.abc();
+				Thread.sleep(192000); // replay
+			} catch(Exception e) {
+
+			}
+		}
 	}
 
 }
