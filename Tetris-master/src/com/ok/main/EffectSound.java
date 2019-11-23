a4fd10cf080d5842355ac348fd73953e84c9ee7d
hyeonazzz <rud0244@gmail.com>
2019-11-23 22:21:22 +0900

    add EffectSound(game_die) git status

---
+++ Tetris-master/src/com/ok/main/EffectSound.java
@@ -0,0 +1,31 @@
+package com.ok.main;
+import java.io.File;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+
+public class EffectSound {
+    public void eff_game_die() {
+        File eff_game_die;
+        AudioInputStream stream;
+        AudioFormat format;
+        DataLine.Info info;
+
+        eff_game_die = new File("Sound/game_die.wav");
+
+        Clip clip;
+
+        try {
+            stream = AudioSystem.getAudioInputStream(eff_game_die);
+            format = stream.getFormat();
+            info = new DataLine.Info(Clip.class, format);
+            clip = (Clip)AudioSystem.getLine(info);
+            clip.open(stream);
+            clip.start();
+        } catch (Exception e) {
+            System.out.println("err : " + e);
+        }
+    }
+}
