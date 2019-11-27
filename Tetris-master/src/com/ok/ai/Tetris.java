5c6591ff036651ac319da1bb8b28723b15cf76e4
Minji-0901 <lmj3772@naver.com>
2019-11-27 14:42:45 +0900

    speed set

--- Tetris-master/src/com/ok/ai/Tetris.java
+++ Tetris-master/src/com/ok/ai/Tetris.java
@@ -205,8 +205,8 @@ public class Tetris
 	public long[] flash;
 	private boolean paused;
 	public int tickCount;
-	public int tickInterval;
-	public int tickThreshold;
+	public int tickInterval;//block speed
+	public int tickThreshold;//timer speed
 	public int ticksPerSecond;
 	public int maxDelays;
 	public int delays;
@@ -225,6 +225,7 @@ public int level=1;
 	}
 	public void setLevel(int level) {
 		this.level=level;
+		this.tickInterval=10/level; //속도 조절
 	}
 	
 	protected PieceGenerator gen;
