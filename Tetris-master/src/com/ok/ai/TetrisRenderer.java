b035fa4b3832da4a23b96e5343ebc8b42526e6bd
KimSeonHu-DGU <fairsh96@gmail.com>
2019-12-10 13:54:31 +0900

    button resize

--- Tetris-master/src/com/ok/ai/TetrisRenderer.java
+++ Tetris-master/src/com/ok/ai/TetrisRenderer.java
@@ -22,6 +22,7 @@ import javax.imageio.*;
 @SuppressWarnings("serial")
 public class TetrisRenderer extends Component implements KeyListener, ActionListener
 {
+	public static Main m = new Main();
 	public static final String VERSION = "1.1";
 	public static int num=0;
 	public static JFrame frame = new JFrame("Tetris");
